ea5690992196644dab27e315c82308e4457e0b58
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-15 13:08:33 +0200

    Implemented task 2.6

---
+++ modules/module-2/task-6/random-codes.py
@@ -0,0 +1,7 @@
+import random
+
+three_digits_code = random.randint(100, 999)
+four_digits_code = random.randint(1111, 9999)
+
+print("Generated three-digit code:", three_digits_code)
+print("Generated Four-digit code:", four_digits_code)
