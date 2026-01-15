3f8cc9bae64af5dfcfefa5cc7b072707e789cc00
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-15 12:18:04 +0200

    Implemented exercise 2.4

---
+++ modules/module-2/task-4/numbers.py
@@ -0,0 +1,11 @@
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+number3 = input("Enter the third number: ")
+
+sum = int(number1) + int(number2) + int(number3)
+multiplication = int(number1) * int(number2) * int(number3)
+average = sum / 3
+
+print("Sum: ", sum)
+print("Multiplication: ", multiplication)
+print("Average: ", average)
