fd82bdc613be7e14b132a9d0fb3e0d536c13decd
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-19 09:29:30 +0200

    fix exercise 2.6

--- modules/module-2/task-6/random-codes.py
+++ modules/module-2/task-6/random-codes.py
@@ -1,6 +1,6 @@
 import random
 
-three_digits_code = random.randint(100, 999)
+three_digits_code = random.randint(111, 999)
 four_digits_code = random.randint(1111, 9999)
 
 print("Generated three-digit code:", three_digits_code)
