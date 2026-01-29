9eac1ddfee5bf0f7264ac81040c1be7102dde9bd
Aleksei Alehin <pixel2006.aa@gmail.com>
2026-01-29 13:55:46 +0200

    Implemented exercise 9.1

---
+++ modules/module-9/task-1/main.py
@@ -0,0 +1,19 @@
+class Car:
+	def __init__(self, register_number, max_speed):
+		self.__register_number = register_number
+		self.__max_speed = max_speed
+		self.__current_speed = 0
+		self.__traveled_distance = 0
+	
+	def print_info(self):
+		print(f"Register number: {self.__register_number}")
+		print(f"Max speed: {self.__max_speed} km/h")
+		print(f"Current speed: {self.__current_speed} km/h")
+		print(f"Traveled distance: {self.__traveled_distance} km")
+
+def main():
+	player = Car("ABC-123", 142)
+
+	player.print_info()
+
+main()
